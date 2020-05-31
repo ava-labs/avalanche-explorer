3b1e931ca39cb5f7451096283fe24142f1401a2d
strictlymomo <momo.araki@gmail.com>
2020-05-31 19:12:33 -0400

    Switch Slopes to Denali testnet

--- src/ava.ts
+++ src/ava.ts
@@ -1,6 +1,6 @@
 import * as slopes from "slopes"
-let AVA_IP = '64.227.61.251';
-let AVA_PORT = 9650;
+let AVA_IP = 'bootstrap.ava.network';
+let AVA_PORT = 21000;
 let AVA_PROTOCOL = 'https';
 let AVA_NETWORK_ID = '3';
 let AVA_CHAIN_ID = 'X';
@@ -8,7 +8,4 @@ let AVA_CHAIN_ID = 'X';
 let ava = new slopes.Slopes(AVA_IP, AVA_PORT, AVA_PROTOCOL, parseInt(AVA_NETWORK_ID), AVA_CHAIN_ID);
 let avm = ava.AVM();
 let platform = ava.Platform();
-console.log("ava", ava);
-console.log(platform.getBlockchains().then(res => res));
-
 export {ava, avm, platform};
