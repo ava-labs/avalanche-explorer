f274c11d8d6ca35c399f0f078ff1e7bd2fc237b6
strictlymomo <momo.araki@gmail.com>
2020-06-18 12:03:25 -0400

    Explorer maintenance banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="home">
+         <v-alert
+            prominent
+            type="warning"
+            id="network-error"
+            >
+            <v-row align="center">
+                <v-col class="grow">Sorry the Explorer is experiencing issues.<br><br>The Denali Network remains operational. You can still <a id="link" href="https://docs.ava.network/v1.0/en/api/avm/#avmgettxstatus">check your transaction activity</a> while we work on resolving these issues.</v-col>
+            </v-row>
+            </v-alert>
         <top-info class="card"></top-info>
         <recent-transactions class="card recent_tx"></recent-transactions>
     </div>
@@ -20,6 +29,15 @@ export default {
 
 <style scoped lang="scss">
 @use "../main";
+
+#network-error {
+    font-weight: bold;
+}
+#link {
+    color: main.$white !important;
+    text-decoration: underline;
+    font-weight: bold;
+}
 
 .home {
     margin: 0px auto;
