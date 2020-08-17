f86b7ecde4e4a402e54bd3a5824e1e9f4e03c7e8
strictlymomo <momo.araki@gmail.com>
2020-08-17 13:28:51 -0400

    Update AvalancheJS and freeze dep versions

--- src/avalanche.ts
+++ src/avalanche.ts
@@ -6,7 +6,7 @@ const NETWORK_ID = "3";
 const CHAIN_ID = "X";
 
 let avalanche = new avalancheJS.Avalanche(IP, PORT, PROTOCOL, parseInt(NETWORK_ID), CHAIN_ID);
-let avm = avalanche.AVM();
-let platform = avalanche.Platform();
+let avm = avalanche.XChain();
+let platform = avalanche.PChain();
 
 export {avalanche, avm, platform};
