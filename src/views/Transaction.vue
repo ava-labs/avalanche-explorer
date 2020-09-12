90afbdf68d9349cc1984d30ea3c50606d825a0c0
strictlymomo <momo.araki@gmail.com>
2020-09-12 11:22:01 -0400

    Add null transaction detail exception

--- src/views/Transaction.vue
+++ src/views/Transaction.vue
@@ -1,9 +1,18 @@
 <template>
     <div class="detail">
         <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
-        <template v-if="!tx">
-            <Loader :contentId="txId" :message="'Fetching Transaction Details'"></Loader>
+        <template v-if="txloading">
+            <Loader :contentId="txID" :message="'Fetching Transaction Details'"></Loader>
         </template>
+        <div v-if="!tx && !txloading" class="card tx_details_error">
+            <h2>Transaction Details Not Found</h2>
+            <p class="message">A record for this transaction ID was not found in the Avalanche Explorer</p>
+            <p class="content_id">
+                {{txID}}
+                <CopyText :value="`${txID}`" class="copy_but"></CopyText>
+            </p>
+            <p><a href="https://github.com/ava-labs/ortelius/issues" target="_blank">Submit Issue</a></p>
+        </div>
         <template v-else>
             <TransactionDetailCard :tx="tx">Transaction Details</TransactionDetailCard>
         </template>
@@ -25,6 +34,7 @@ import api from "../axios";
     }
 })
 export default class TransactionPage extends Vue {
+    txloading: boolean = false;
     tx: Transaction | null = null;
     breadcrumbs: any = [
         {
@@ -39,8 +49,8 @@ export default class TransactionPage extends Vue {
         }
     ];
 
-    @Watch("txId")
-    ontxIdChanged(val: string, oldVal: string) {
+    @Watch("txID")
+    ontxIDChanged(val: string, oldVal: string) {
         this.getData();
     }
 
@@ -57,22 +67,22 @@ export default class TransactionPage extends Vue {
         return this.$store.state.assetsLoaded;
     }
 
-    get txId(): string {
+    get txID(): string {
         return this.$route.params.id;
     }
 
     getData(): void {
-        let parent = this;
+        this.txloading = true;
+        
         // TODO: support service for multiple chains
-        let url = `/x/transactions/${this.txId}`;
+        let url = `/x/transactions/${this.txID}`;
         if (this.assetsLoaded) {
-            api.get(url)
-            .then(res => {
-                const data = res.data;
-                // console.log(data);
-                let tx = new Transaction(data);
-                parent.tx = tx;
-                // console.log(tx);
+            api.get(url).then(res => {
+                this.txloading = false;
+                if (res.data) {
+                    // tx in Ortelius
+                    this.tx = new Transaction(res.data);
+                }
             })
             .catch(err => {
                 console.log(err);
@@ -84,4 +94,53 @@ export default class TransactionPage extends Vue {
 
 <style scoped lang="scss">
 @use "../main";
+
+.tx_details_error {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;    
+    text-align: center;
+
+    h2 {
+        font-weight: 400; /* 700 */
+        font-size: 36px;
+        line-height: 1.25em;
+    }
+
+    .message {
+        margin: 12px 0 0.5em;
+        font-size: 16px;
+        color: main.$primary-color;
+    }
+
+    .content_id {
+        font-size: 16px;
+        color: main.$primary-color-light;
+    }
+
+    a {
+        display: block;
+        width: max-content;
+        text-decoration: none !important;
+        margin-top: 30px;
+        transition: opacity 0.3s;
+        
+        background-color: transparent !important;
+        color: main.$secondary-color !important;
+        padding: 10px 24px;
+
+        border-radius: 6px;
+        font-family: "DM Sans", sans-serif;
+        font-weight: 700;
+        letter-spacing: .5px;
+        text-transform: uppercase!important;
+        font-size: 14px;
+
+        &:hover {
+            opacity: 0.9;
+        }
+
+    }
+}
 </style>
