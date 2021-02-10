78c726905a64effea3a16aaf4a5800e6224b2811
strictlymomo <momo.araki@gmail.com>
2021-02-10 15:07:59 -0500

    Mainnet Alert: Network Load

--- src/components/misc/TestnetAlert.vue
+++ src/components/misc/TestnetAlert.vue
@@ -1,9 +1,17 @@
 <template>
     <div>
         <template v-if="networkID === 1">
-            <!-- <v-alert class="testnet_alert mainnet_announcement" rounded="0">
-                <p class="description">Per consensum ad astra</p>
-            </v-alert> -->
+            <v-alert
+                class="testnet_alert mainnet_announcement"
+                text
+                rounded="0"
+            >
+                <p class="description">
+                    Notice: Due to very high network load, the Explorer is
+                    currently experiencing difficulties.
+                </p>
+                <!-- <p class="description">Per consensum ad astra</p> -->
+            </v-alert>
         </template>
         <template v-else>
             <v-alert class="testnet_alert" text type="info" rounded="0">
@@ -51,15 +59,16 @@ export default class TestnetAlert extends Vue {
         to left,
         #ff8080,
         #f6a06a,
-        #debf6f,
-        #c2da8f,
-        #a9efbf
+        #debf6f
     ) !important;
 
     .description {
         text-align: center;
+        font-weight: bold;
     }
 }
+// #c2da8f
+// #a9efbf
 
 @if $VUE_APP_DEFAULT_NETWORKID == 1 {
     .v-alert--text:before {
