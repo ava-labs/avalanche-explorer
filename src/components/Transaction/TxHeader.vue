d226fdc14445beeeed2d97bcc10137e3829dc5fe
strictlymomo <momo.araki@gmail.com>
2021-02-23 12:40:30 -0500

    Remove Viewing X-Chain txs from TxHeader

--- src/components/Transaction/TxHeader.vue
+++ src/components/Transaction/TxHeader.vue
@@ -1,19 +1,6 @@
 <template>
     <div class="tx_chain_header">
         <h2>Transactions</h2>
-        <p v-if="$vuetify.breakpoint.smAndUp" class="chain right" bottom>
-            <span class="label">You are viewing transactions for</span>
-            <v-tooltip>
-                <template v-slot:activator="{ on }">
-                    <span class="tag" v-on="on">X-Chain</span>
-                </template>
-                <span
-                    >The X-Chain acts as a decentralized platform for creating
-                    and trading smart digital assets. (Think X for eXchanging
-                    assets.)</span
-                >
-            </v-tooltip>
-        </p>
     </div>
 </template>
 <script lang="ts">
