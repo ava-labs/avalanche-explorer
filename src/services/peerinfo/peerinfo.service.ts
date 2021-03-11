19f7f01fdda0475246856e2250efd28b0be82e25
strictlymomo <momo.araki@gmail.com>
2021-03-11 16:41:02 -0500

    refactor: 💡 Parse new response for peer info metrics

--- src/services/peerinfo/peerinfo.service.ts
+++ src/services/peerinfo/peerinfo.service.ts
@@ -23,40 +23,43 @@ export async function getPeerInfo() {
     )) as string
 
     // convert string to structured data
-    let peerInfo: IVersion[] = res
-        .split('peerinfo')
+    const peerMap: any = {}
+    res.split('peerinfo_')
         .filter((x) => !!x)
-        .map((y) => {
-            return y
-                .slice(1, -1)
-                .split(',')
-                .reduce((acc, curr) => {
-                    return {
-                        ...acc,
-                        [curr.split('=')[0]]: curr.split('=')[1],
-                    }
-                }, {})
-        })
-        .map((z: any) => {
-            return {
-                version: removePrefix(z.version.slice(1, -1)),
-                nodeCount: parseInt(z.nodeCount),
-                stakeAmount: toAVAX(z.stakeAmount),
+        .map((y) => y.split('{version="'))
+        .forEach((peer) => {
+            const datum = removePrefix(peer[1])
+                .replace(/(\r\n|\n|\r)/gm, '')
+                .split(`"} `)
+            if (peer[0] === 'nodecount') {
+                peerMap[datum[0]] = {
+                    version: datum[0],
+                    nodeCount: parseInt(datum[1]),
+                }
+            } else {
+                peerMap[datum[0]].stakeAmount = toAVAX(datum[1])
             }
         })
+
+    // create list for charts
+    let peerInfo: IVersion[] = []
+    for (const peer in peerMap) {
+        peerInfo.push(peerMap[peer])
+    }
+
+    const totalStake = getTotalStake(peerInfo)
+
+    peerInfo = peerInfo
         .sort((a, b) =>
             a.version.localeCompare(b.version, undefined, { numeric: true })
         )
         .reverse()
-
-    const totalStake = getTotalStake(peerInfo)
-
-    peerInfo = peerInfo.map((peer) => {
-        return {
-            stakePercent: Math.round((peer.stakeAmount / totalStake) * 100),
-            ...peer,
-        }
-    })
+        .map((peer) => {
+            return {
+                stakePercent: Math.round((peer.stakeAmount / totalStake) * 100),
+                ...peer,
+            }
+        })
 
     // move 'offline' nodes to end of array
     const offline = peerInfo.shift() as IVersion
