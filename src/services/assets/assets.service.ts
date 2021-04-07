20309329f7c3922bf2c437a567aec6b7b30bcdbc
strictlymomo <momo.araki@gmail.com>
2021-04-07 12:04:06 -0400

    Cache assets service

--- src/services/assets/assets.service.ts
+++ src/services/assets/assets.service.ts
@@ -3,6 +3,13 @@ import { resolveResponseData } from '@/services/helpers'
 
 const ASSETS_V2_API_BASE_URL = process.env.VUE_APP_ASSETS_V2_API_BASE_URL
 
+const CACHE_ASSETS_V2_API_BASE_URL =
+    process.env.VUE_APP_CACHE_ASSETS_V2_API_BASE_URL
+
 export function getAssetInfo(id: string) {
     return api.get(`${ASSETS_V2_API_BASE_URL}/${id}`).then(resolveResponseData)
+}
+
+export function getCacheAssets() {
+    return api.get(`${CACHE_ASSETS_V2_API_BASE_URL}`).then(resolveResponseData)
 }
