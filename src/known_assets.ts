4fa7216750c178bb667cd62ee5f10aa36bc494ae
strictlymomo <momo.araki@gmail.com>
2021-05-25 14:02:06 -0400

    known asset icon

--- src/known_assets.ts
+++ src/known_assets.ts
@@ -7,6 +7,11 @@ export const AVAX_ID =
         ? (process.env.VUE_APP_AVAXID as string)
         : (process.env.VUE_APP_TEST_AVAXID as string)
 
+export const BAGUETTE_ID =
+    DEFAULT_NETWORK_ID === 1
+        ? 'F4MyJcUvq3Rxbqgd4Zs8sUpvwLHApyrp4yxJXe2bAV86Vvp38'
+        : undefined
+
 interface AssetDict {
     [key: string]: {
         logo: string
@@ -17,6 +22,12 @@ const dict: AssetDict = {}
 
 dict[AVAX_ID] = {
     logo: 'img/avax_icon_circle.png',
+}
+
+if (BAGUETTE_ID) {
+    dict[BAGUETTE_ID] = {
+        logo: 'img/baguette_icon_circle.jpg',
+    }
 }
 
 export default dict
