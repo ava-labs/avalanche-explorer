6305001ed89691c03356d53a2f5934a24335fe0a
strictlymomo <momo.araki@gmail.com>
2021-06-17 16:18:15 -0400

    Mobile interactive tx

--- src/components/Transaction/TxInteractive.vue
+++ src/components/Transaction/TxInteractive.vue
@@ -1,8 +1,29 @@
 <template>
     <div>
         <!-- MOBILE -->
+        <template v-if="$vuetify.breakpoint.smAndDown">
+            <v-col cols="12" class="py-2">
+                <div class="buttons">
+                    <TxParamsDialog @change="emitChange" />
+                    <TxFilterDialog :chains="chains" @change="setFilter" />
+                </div>
+            </v-col>
+
+            <TxList
+                v-if="!loading && assetsLoaded"
+                :transactions="filteredTransactions"
+            />
+            <v-progress-circular
+                v-else
+                key="1"
+                :size="16"
+                :width="2"
+                color="#E84970"
+                indeterminate
+            />
+        </template>
         <!-- DESKTOP -->
-        <template>
+        <template v-else>
             <TxParams @change="emitChange" />
             <div class="two-col">
                 <TxFilter :chains="chains" @change="setFilter" />
@@ -28,8 +49,10 @@
 <script lang="ts">
 import 'reflect-metadata'
 import { Vue, Component, Prop } from 'vue-property-decorator'
+import TxParamsDialog from '@/components/Transaction/TxParamsDialog.vue'
 import TxParams from '@/components/Transaction/TxParams.vue'
 import TxList from '@/components/Transaction/TxList.vue'
+import TxFilterDialog from '@/components/Transaction/TxFilterDialog.vue'
 import TxFilter from '@/components/Transaction/TxFilter.vue'
 import { ChainMap } from '@/known_blockchains'
 import { Transaction } from '@/js/Transaction'
@@ -37,7 +60,9 @@ import { ITransactionParams } from '@/services/transactions'
 
 @Component({
     components: {
+        TxParamsDialog,
         TxParams,
+        TxFilterDialog,
         TxFilter,
         TxList,
     },
@@ -47,7 +72,7 @@ export default class TxInteractive extends Vue {
     @Prop() assetsLoaded!: boolean
     @Prop() loading!: boolean
     @Prop() chains!: ChainMap[]
-
+    text = 'center'
     filters: string[] = []
 
     setFilter(val: string[]) {
@@ -83,5 +108,10 @@ export default class TxInteractive extends Vue {
     .right {
         flex: 1;
     }
+}
+
+.buttons {
+    display: flex;
+    justify-content: space-between;
 }
 </style>
---
+++ src/components/Transaction/TxParamsDialog.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-row justify="center">
+        <v-dialog
+            v-model="dialog"
+            fullscreen
+            hide-overlay
+            transition="dialog-bottom-transition"
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on" icon color="pink">
+                    <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+            </template>
+            <v-card>
+                <!-- TOOLBAR -->
+                <v-toolbar>
+                    <v-btn icon @click="dialog = false">
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                    <v-toolbar-title>Search</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                </v-toolbar>
+                <!-- BODY -->
+                <v-list three-line subheader>
+                    <TxParams @change="emitChange" />
+                </v-list>
+            </v-card>
+        </v-dialog>
+    </v-row>
+</template>
+
+<script lang="ts">
+import 'reflect-metadata'
+import { Vue, Component } from 'vue-property-decorator'
+import TxParams from '@/components/Transaction/TxParams.vue'
+import { ITransactionParams } from '@/services/transactions'
+
+@Component({
+    components: { TxParams },
+})
+export default class TxParamsDialog extends Vue {
+    dialog = false
+    notifications = false
+    sound = true
+    widgets = false
+
+    // simply passes value through to the data request layer above
+    emitChange(params: ITransactionParams): void {
+        this.$emit('change', params)
+        this.dialog = false
+    }
+}
+</script>
+
+<style scoped lang="scss"></style>
