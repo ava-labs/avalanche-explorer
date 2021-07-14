7c2b0a81af152165f4d2ac7f3954f3d466b2a6c1
strictlymomo <momo.araki@gmail.com>
2021-07-14 18:48:44 -0400

    treeview remove select

--- src/components/Transaction/EVMExecutionTrace.vue
+++ src/components/Transaction/EVMExecutionTrace.vue
@@ -2,10 +2,6 @@
     <div>
         <v-treeview
             v-model="items"
-            selectable
-            :selection-type="'leaf'"
-            selected-color="#000"
-            item-disabled="locked"
             :items="items"
             return-object
             open-all
@@ -25,25 +21,9 @@ export default class EVMExecutionTrace extends Vue {
     @Prop() traces!: any[]
 
     items: any[] = []
-    selection: any[] = []
 
     created() {
         this.items = this.traces
-
-        this.selection = this.items.flatMap((item) => item.children)
-
-        this.$emit(
-            'change',
-            this.selection.map((val) => val.id)
-        )
-    }
-
-    @Watch('selection')
-    onSelectionChanged() {
-        this.$emit(
-            'change',
-            this.selection.map((val) => val.id)
-        )
     }
 }
 </script>
